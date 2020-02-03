1e6907519d2c28a8945d987aca00a92300176300
Ryan McKirdy <rmckirdy@monmouthcollege.edu>
2020-02-03 15:49:49 -0600

    Added qisket implementation of Hello Quantum level 1
    In this directory, added hq_1_1.py, following model of implementation used in cirq

---
+++ HelloQuantum/qiskit/hq_1_1.py
@@ -0,0 +1,43 @@
+"""
+Ryan McKirdy
+
+Implementation of Hello Quantum level 1 in qiskit
+"""
+
+from qiskit import(
+  QuantumCircuit,
+  execute,
+  Aer)
+from qiskit.visualization import plot_histogram
+
+#%%
+
+# Use Aer's qasm_simulator
+simulator = Aer.get_backend('qasm_simulator')
+
+# Create a Quantum Circuit acting on the q register
+circuit = QuantumCircuit(2)
+
+#%%
+
+# Add a X gate on qubit 0
+circuit.x(1)
+circuit.x(0)
+
+# Map the quantum measurement to the classical bits
+circuit.measure_all()
+print(circuit.draw())
+
+#%%
+
+# Execute the circuit on the qasm simulator
+job = execute(circuit, simulator, shots=100)
+
+# Grab results from the job
+result = job.result()
+
+# Returns counts
+counts = result.get_counts(circuit)
+print("\nTotal count:",counts)
+plot_histogram(counts).show()
+# Draw the circuit
